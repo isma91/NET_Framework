786333f268d0b8a6df02694df6482234aa3bf11e
Ismail Aydogmus <ismaydogmus@gmail.com>
2016-07-28 14:36:04 +0200

    set the notifyChange event in the View model

--- NET_Framework/ProductViewModel.cs
+++ NET_Framework/ProductViewModel.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NET_Framework
 {
-    public class ProductViewModel
+    public class ProductViewModel : INotifyPropertyChanged
     {
         private ObservableCollection<Product> productList = new ObservableCollection<Product>();
-        public ObservableCollection<Product> ProductList { get { return this.productList; } }
+        public ObservableCollection<Product> ProductList
+        {
+            get
+            {
+                return this.productList;
+            }
+
+            set
+            {
+                this.NotifyPropertyChanged("ProductList");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public void addInList(List<Product> list)
         {
@@ -18,6 +32,14 @@ namespace NET_Framework
             foreach (var product in list)
             {
                 this.productList.Add(product);
+            }
+        }
+
+        private void NotifyPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
